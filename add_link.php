6845685b095c23e59652f0657d7816619b1aa08d
truongvando <111683799+truongvando@users.noreply.github.com>
2023-06-23 03:24:15 +0700

    Update add_link.php

--- add_link.php
+++ add_link.php
@@ -4,6 +4,10 @@ include 'db_connection.php';
 
 $link = $_POST['link'];
 $user_id = $_SESSION['user_id']; // Lấy user_id từ session
+$notes = $_POST['notes'];
+
+$sql = "INSERT INTO links (id, link, percentage, user_id, notes) VALUES (?, ?, 0, ?, ?)";
+$stmt->bind_param('isii', $new_id, $link, $user_id, $notes);
 
 // Tìm ID lớn nhất trong bảng
 $sql = "SELECT MAX(id) as max_id FROM links";
